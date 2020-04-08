57b75c8f60446b1700210fb74a5f70360164c75f
sergioprgarcia <serjinn@gmail.com>
2020-04-08 16:59:04 -0300

    Commit Inicial - BlueTasks

---
+++ bluetasks-backend/src/test/java/br/com/sergio/bluetasks/BluetasksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sergio.bluetasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluetasksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
